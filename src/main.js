dc248e762699665d1f03b776c6fa16022c10b854
Mahbub <prof.bub@live.com>
2019-03-21 21:27:24 +0700

    config process.env

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ Vue.config.productionTip = false;
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCknkfmA01w2VNKR0HCQ9JptsIZx8D077I",
-  authDomain: "translate-racer.firebaseapp.com",
-  databaseURL: "https://translate-racer.firebaseio.com",
-  projectId: "translate-racer",
-  storageBucket: "translate-racer.appspot.com",
-  messagingSenderId: "523628763033"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 
